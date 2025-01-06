5b3ee9c8193654c131deb892786fcbe92999a1ba
tuffffry <karrotop72@gmail.com>
2025-01-06 15:51:36 +0530

    RustHub.lua

--- RustHub.lua
+++ RustHub.lua
@@ -1,10 +1,10 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://[Log in to view URL]'))()
 local Window = Rayfield:CreateWindow({
    Name = "Rust | Hub",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "RustHub Loading...",
    LoadingSubtitle = "By timburrig",
-   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+   Theme = "Amethyst", -- Check https://[Log in to view URL]
 
    DisableRayfieldPrompts = false,
    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
@@ -29,6 +29,9 @@ local Window = Rayfield:CreateWindow({
       FileName = "Keygfgufcjhsecbivseh", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/keQebBGM"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
+      Key = {"https://[Log in to view URL]"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+            
+            local Tab = Window:CreateTab("Main", "album")
+            
+      }
+      
