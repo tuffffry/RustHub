06401bb5817031fc5def57d908cd35fd14295401
tuffffry <karrotop72@gmail.com>
2025-01-06 18:09:19 +0530

    RustHub.lua

--- RustHub.lua
+++ RustHub.lua
@@ -10,13 +10,13 @@ local Window = Rayfield:CreateWindow({
    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
    ConfigurationSaving = {
-      Enabled = true,
+      Enabled = false,
       FolderName = saves, -- Create a custom folder for your hub/game
       FileName = "RustHub"
    },
 
    Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
       Invite = "djsA2S2SCT", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
@@ -29,11 +29,8 @@ local Window = Rayfield:CreateWindow({
       FileName = "Keygfgufcjhsecbivseh", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/keQebBGM"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-            
-            local Tab = Window:CreateTab("Main", "album")
-            
-            Rayfield:LoadConfiguration()
+      Key = {"https://pastebin.com/raw/keQebBGM"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22"
    }
 })
+local Tab = Window:CreateTab("Main", "Album")
       
